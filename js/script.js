f807f981aac8930e8189274376b849815c5e0330
YousefElsebaiy <145772263+YousefElsebaiy@users.noreply.github.com>
2024-07-26 18:51:51 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-let animation = bodymovin.loadAnimation({
-    container: document.getElementsByClassName("right")[0],
-    render: "svg",
-    loop: true,
-    autoplay: true,
-    path: "../json/Animation - 1721822021638.json"
-})
 window.addEventListener("load", () => {
     let load = document.querySelector(".load")
     load.classList.add("load-hidden")
